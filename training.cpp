1520f1498523ea739cfa85614bbae3ed844e8c9b
Marcus <marcus.greting@gmail.com>
2025-10-24 15:17:49 +0200

    updated training.cpp
    implemented constructor for training class

--- training.cpp
+++ training.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <string>
 
 using namespace std;
 
@@ -11,13 +12,42 @@ map<char, int> char_count;
 map<string,int> kgrams;
     
 public:
-    training(string filename);
+    training(string filename,int k);
    map<char,int> get_char_count();
     map<string,int> get_kgrams();
 };
 
-training::training(string filename)
+training::training(string filename,int k)
 {
+    string line; 
+    ifstream file(filename);
+     
+    while (getline(file,line)){
+        for (char c: line){
+            if (training::char_count.count(c))
+            {
+                training::char_count[c] ++;
+            }
+            else
+            {
+                training::char_count[c] == 1;
+            }
+            
+
+        }
+        for (int i; (i<line.size() and i+k <line.size()); i++){
+           string kgram = line.substr(i,k);
+           if (training::kgrams.count(kgram))
+           {
+               training::kgrams[kgram]++;
+           }
+           else
+           {
+               training::kgrams[kgram] = 1;
+           }
+           
+        }
+    }
 }
 
 
